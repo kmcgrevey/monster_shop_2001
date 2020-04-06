2b088f39f1c1378eb3feb848b7bb97c825cbf097
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-06 16:32:40 -0600

    Create user spec test combo pack

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
-end 
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :name, :address, :city, :state, :zip, :email, :password, :password_confirmation
+
+  has_secure_password
+end
