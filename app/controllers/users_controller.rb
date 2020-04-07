049504b2af19be1aee193dc4e2087a175ccc74f0
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-06 19:45:30 -0600

    Get error message to show when fields are not fully entered

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,9 @@ class UsersController < ApplicationController
     if user.save
       flash[:success]= "You are now registered and logged in!"
       redirect_to "/profile"
+    else
+      flash[:error] = "You are missing required fields."
+      redirect_back(fallback_location: "/")
     end
   end
 
