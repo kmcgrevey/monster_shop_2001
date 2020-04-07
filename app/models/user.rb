222e25720ff22c42b85ce97c88ba31f1a24cdfb2
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-07 15:46:22 -0600

    Adds role column to User and test for user admin and employee

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   has_secure_password
   validates_presence_of :name, :address, :city, :state, :zip, :password_confirmation
+
+  enum role: {user: 0, employee: 1, admin: 2}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,51 @@ RSpec.describe User do
 
     it {should validate_uniqueness_of(:email)}
   end
+  describe "roles" do
+    it "can be created as a default user" do
+        josh = User.create!(name: "Josh Tukman",
+                              address: "756 Main St",
+                              city: "Denver",
+                              state: "Colorado",
+                              zip: "80209",
+                              email: "josh.t@gmail.com",
+                              password: "secret_password",
+                              password_confirmation: "secret_password")
+
+        expect(josh.role).to eq("user")
+        expect(josh.user?).to be_truthy
+    end
+
+    it "can be created as an employee" do
+        josh = User.create!(name: "Josh Tukman",
+                        address: "756 Main St",
+                        city: "Denver",
+                        state: "Colorado",
+                        zip: "80209",
+                        email: "josh.t@gmail.com",
+                        password: "secret_password",
+                        password_confirmation: "secret_password",
+                        role: 1)
+
+        expect(josh.role).to eq("employee")
+        expect(josh.employee?).to be_truthy
+    end
+    
+    it "can be created as an admin" do
+        josh = User.create!(name: "Josh Tukman",
+                        address: "756 Main St",
+                        city: "Denver",
+                        state: "Colorado",
+                        zip: "80209",
+                        email: "josh.t@gmail.com",
+                        password: "secret_password",
+                        password_confirmation: "secret_password",
+                        role: 2)
+
+        expect(josh.role).to eq("admin")
+        expect(josh.admin?).to be_truthy
+    end
+  end
+
+
 end
