fce7438e06893ff42eb509a105f5c280e3d89d3d
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-06 21:49:39 -0600

    Remove double validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :name, :address, :city, :state, :zip, :email, :password, :password_confirmation
-
   has_secure_password
+  validates_presence_of :name, :address, :city, :state, :zip, :password_confirmation
 end
