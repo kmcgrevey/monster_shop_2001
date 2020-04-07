59cb5d834d23d7a47a48b9179142c5280aa31a6b
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-06 19:58:17 -0600

    Create test for flash error if email is not unique

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -46,6 +46,36 @@ RSpec.describe "As a visitor" do
         within ".error-flash" do
           expect(page).to have_content("You are missing required fields.")
         end
+
+      it "if I fill out a registration form with an email already in the system" do
+        user_1 = User.create!(name: "Josh Tukman",
+                              address: "756 Main St",
+                              city: "Denver",
+                              state: "Colorado",
+                              zip: "80209",
+                              email: "josh.t@gmail.com",
+                              password: "secret_password",
+                              password_confirmation: "secret_password")
+
+        visit "/register"
+          fill_in :name, with: "Mike Hernandez"
+          fill_in :address, with: "25 Cowboy Way"
+          fill_in :city, with: "Denver"
+          fill_in :state, with: "Colorado"
+          fill_in :zip, with: "80121"
+          fill_in :email, with: "josh.t@gmail.com"
+          fill_in :password, with: "cowboy_password"
+          fill_in :password_confirmation, with: "cowboy_password"
+
+          click_button "Register"
+
+          expect(current_path).to eq("/register")
+
+          within ".error-flash" do
+            expect(page).to have_content("The email you entered is already in use.")
+          end
+
+      end
     end
   end
 end
