19096c7968582cd49c7351feb39b0ad07c9aa629
“Krista <stadler.krista@gmail..com>
2020-04-08 08:44:10 -0600

    Fixed application controller current user method to use session[:user_id]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       flash[:success]= "You are now registered and logged in!"
-      session[:user_info] = {role: user.role, id: user.id}
+      session.clear
+      session[:user_id] = user.id
       redirect_to "/default/profile"
     else
       flash[:error] = user.errors.full_messages.to_sentence
