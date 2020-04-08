6ba4a4b88e0bc3f3a69d6b314d0a32c54f3dc771
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-07 21:27:26 -0600

    Adds test for admin navigation links

---
+++ spec/features/admin/navigation_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe "As an Admin", type: :feature do
+  describe "within the nav bar" do
+    before(:each) do
+      @user_1 = User.create!(name: "Josh Tukman",
+                            address: "756 Main St",
+                            city: "Denver",
+                            state: "Colorado",
+                            zip: "80209",
+                            email: "josh.t@example.com",
+                            password: "secret_password",
+                            password_confirmation: "secret_password",
+                            role: 2)
+      
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return("admin")
+
+      visit "/merchants"
+    end
+    
+    it "I see the same links as a regular user" do
+      
+      within ".topnav" do
+        click_link "Profile" 
+      end
+      
+      expect(current_path).to eq("/profile")
+      
+      within ".topnav" do
+        click_link "All Merchants" 
+      end
+      
+      expect(current_path).to eq("/merchants")
+      
+      within ".topnav" do
+        click_link "All Items" 
+      end
+      
+      expect(current_path).to eq("/items")
+      
+      within ".topnav" do
+        click_link "Logout"
+      end
+    end
+
+    it "I also see my admin only links" do
+
+      within ".topnav" do
+        click_link "Admin Dashboard" 
+      end
+      
+      expect(current_path).to eq("/admin")
+
+      within ".topnav" do
+        click_link "All Users" 
+      end
+      
+      expect(current_path).to eq("/admin/users")
+    end
+
+
+  end
+end
+
