945d9feac990649c66a083329fe0ba8efb8e94fd
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-08 20:41:18 -0600

    Create model method for user knowing its info

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, :password_confirmation
 
   enum role: {default: 0, merchant: 1, admin: 2}
+
+  def info
+    {name: name, address: address, city: city, state: state, zip: zip, email: email}
+  end
 end
