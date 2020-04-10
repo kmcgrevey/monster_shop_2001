9989961e8540306fabaabced10b6fffce9499267
“Krista <stadler.krista@gmail..com>
2020-04-09 19:47:08 -0600

    Wrote tests for User Story 26

--- spec/features/cart/checkout_spec.rb
+++ spec/features/cart/checkout_spec.rb
@@ -16,6 +16,18 @@ RSpec.describe 'Cart show' do
       visit "/items/#{@pencil.id}"
       click_on "Add To Cart"
       @items_in_cart = [@paper,@tire,@pencil]
+
+      @user = User.create!(name: "Josh Tukman",
+                            address: "756 Main St.",
+                            city: "Denver",
+                            state: "Colorado",
+                            zip: "80209",
+                            email: "josh.t@gmail.com",
+                            password: "secret_password",
+                            password_confirmation: "secret_password",
+                            role: 0)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
     end
 
     it 'Theres a link to checkout' do
@@ -27,7 +39,34 @@ RSpec.describe 'Cart show' do
 
       expect(current_path).to eq("/orders/new")
     end
+
+    it "I can complete an order form and place my order" do
+      visit "/orders/new"
+
+      fill_in :name, with: @user.name
+      fill_in :address, with: @user.address
+      fill_in :city, with: @user.city
+      fill_in :state, with: @user.state
+      fill_in :zip, with: @user.zip
+      click_button "Create Order"
+
+      expect(current_path).to eq("/profile/orders")
+
+      order = Order.last
+
+      expect(order.name).to eq("Josh Tuckman")
+      expect(order.status).to eq("Pending")
+      
+      within ".success-flash" do
+        expect(page).to have_content("Your order was created!")
+      end
+      expect(page).to have_content(order.id)
+
+      visit '/cart'
+      expect(page).to have_content("Cart is currently empty")
+    end
   end
+
 
   describe 'When I havent added items to my cart' do
     it 'There is not a link to checkout' do
