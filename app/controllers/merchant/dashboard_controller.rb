fb3a5be823545b2d18ef58a193d900c6efcb6ae7
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-11 21:28:04 -0600

    Fixes merge conflict in dashboard controller

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,17 +1,8 @@
 class Merchant::DashboardController < Merchant::BaseController
   before_action :require_merchant
 
-<<<<<<< HEAD
-  # def index
-  #   binding.pry
-  #   @merchant = Merchant.find(current_user.merchant_id.to_s)
-  # end
-
-  def show
-=======
   def show
     #@user = current_user
->>>>>>> fadd900b229fd62eed0283cecebfad01824da738
     @merchant = Merchant.find(current_user.merchant_id)
   end
 
