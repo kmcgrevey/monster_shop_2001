aa184af40508b91c89f0596adbce871548b01bb7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-13 16:07:33 -0600

    Fix merge issues

--- spec/features/merchant_dashboard/show_spec.rb
+++ spec/features/merchant_dashboard/show_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe "As a merchant employee", type: :feature do
       @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @seat = @bike_shop.items.create!(name: "Seat", description: "Cushy for your tushy.", price: 199, image: "https://www.rei.com/media/product/153242", inventory: 20)
       @pump = @bike_shop.items.create!(name: "Pump", description: "Not just hot air", price: 70, image: "https://www.rei.com/media/product/152974", inventory: 20)
-      
+
       @pull_toy = @dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-     
+
       josh = @bike_shop.users.create!(name: "Josh Tukman",
                             address: "756 Main St",
                             city: "Denver",
@@ -24,16 +24,16 @@ RSpec.describe "As a merchant employee", type: :feature do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(josh)
     end
-    
+
     it "can show the name and full address of the merchant that I work for" do
       visit "/merchant"
 
       expect(page).to have_content("Brian's Bike Shop")
       expect(page).to have_content("Address: #{@bike_shop.address}")
-      expect(page).to have_content("Address: #{@bike_shop.city}")
-      expect(page).to have_content("Address: #{@bike_shop.state}")
-      expect(page).to have_content("Address: #{@bike_shop.zip}")
-      
+      expect(page).to have_content("City: #{@bike_shop.city}")
+      expect(page).to have_content("State: #{@bike_shop.state}")
+      expect(page).to have_content("Zip: #{@bike_shop.zip}")
+
       expect(page).to_not have_content("Meg's Dog Shop")
     end
 
@@ -74,7 +74,7 @@ RSpec.describe "As a merchant employee", type: :feature do
                             city: "Denver",
                             state: "Colorado",
                             zip: "80210",
-                            email: "josh.t@gmail.com",
+                            email: "josh@gmail.com",
                             password: "secret_password",
                             password_confirmation: "secret_password",
                             role: 1)
@@ -108,7 +108,7 @@ RSpec.describe "As a merchant employee", type: :feature do
       expect(page).to have_content("#{@tire.name}")
       expect(page).to have_content("#{@seat.name}")
       expect(page).to have_content("#{@pump.name}")
-      
+
       expect(page).not_to have_content("#{@dog_shop.name}")
       expect(page).not_to have_content("#{@pull_toy.name}")
     end
