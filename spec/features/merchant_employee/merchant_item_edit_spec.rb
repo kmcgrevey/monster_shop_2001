d453817407e1fb504408187139cfa17ff997292d
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-14 19:36:00 -0600

    Change verbage of test to be a little more clear

--- spec/features/merchant_employee/merchant_item_edit_spec.rb
+++ spec/features/merchant_employee/merchant_item_edit_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "as a merchant employee when I visit my items page", type: :feature do
-  # As a merchant employee
-  # When I visit my items page
+
   before(:each) do
     @bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
@@ -20,17 +19,10 @@ RSpec.describe "as a merchant employee when I visit my items page", type: :featu
 
     @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
     @stud = @bike_shop.items.create(name: "Canti Studs", description: "You don't need 'em till you do.'", price: 5, image: "https://www.jensonusa.com/globalassets/product-images---all-assets/problem-solvers/br309z00.jpg", active?:false, inventory: 4)
-    # @seat = @bike_shop.items.create(name: "Seat", description: "Cushy for your tushy.", price: 199, image: "https://www.rei.com/media/product/153242", inventory: 20)
-    # @pump = @bike_shop.items.create(name: "Pump", description: "Not just hot air", price: 70, image: "https://www.rei.com/media/product/152974", inventory: 20)
-    # @pedals = @bike_shop.items.create(name: "Pedals", description: "Clipless bliss!", price: 210, image: "https://www.rei.com/media/product/130015", inventory: 20)
-    # @helmet = @bike_shop.items.create(name: "Helmet", description: "Safety Third!", price: 100, image: "https://www.rei.com/media/product/153004", inventory: 20)
   end
 
   describe "when I click the edit button next to any item" do
-    # And I click the edit button or link next to any item
     it "I am taken to a pre-populated form with all of the item's information" do
-      # Then I am taken to a form similar to the 'new item' form
-      # The form is pre-populated with all of this item's information
       visit "/merchant/items"
 
       within "#item-#{@tire.id}" do
@@ -55,7 +47,7 @@ RSpec.describe "as a merchant employee when I visit my items page", type: :featu
 
     describe "I can change the information but all the rules for new items still apply" do
 
-      it "has to have a name" do
+      it "name can not be blank" do
         visit "/merchant/items/#{@stud.id}/edit"
 
         fill_in "Name", with: ""
@@ -66,7 +58,7 @@ RSpec.describe "as a merchant employee when I visit my items page", type: :featu
         expect(page).to have_content("Name can't be blank")
       end
 
-      it "has to have a description" do
+      it "description can not be blank" do
         visit "/merchant/items/#{@stud.id}/edit"
 
         fill_in "Description", with: ""
@@ -124,7 +116,6 @@ RSpec.describe "as a merchant employee when I visit my items page", type: :featu
   end
 
     it "if I don't supply an image, a default thumbnail is in its place" do
-      # If I left the image field blank, I see a placeholder image for the thumbnail
       visit "/merchant/items/#{@stud.id}/edit"
 
       fill_in "Name", with: "Canti Stud"
