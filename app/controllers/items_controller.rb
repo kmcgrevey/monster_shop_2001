5325d8d499a8c965e356f754271376d85c7bade0
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-15 15:03:05 -0600

    Begin merging in other user stories

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,21 +12,21 @@ class ItemsController<ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-
-  def new
-    @merchant = Merchant.find(params[:merchant_id])
-  end
-
-  def create
-    @merchant = Merchant.find(params[:merchant_id])
-    item = @merchant.items.create(item_params)
-    if item.save
-      redirect_to "/merchants/#{@merchant.id}/items"
-    else
-      flash[:error] = item.errors.full_messages.to_sentence
-      render :new
-    end
-  end
+  #
+  # def new
+  #   @merchant = Merchant.find(params[:merchant_id])
+  # end
+  #
+  # def create
+  #   @merchant = Merchant.find(params[:merchant_id])
+  #   item = @merchant.items.create(item_params)
+  #   if item.save
+  #     redirect_to "/merchants/#{@merchant.id}/items"
+  #   else
+  #     flash[:error] = item.errors.full_messages.to_sentence
+  #     render :new
+  #   end
+  # end
 
   # def edit
   #   @item = Item.find(params[:id])
@@ -44,18 +44,18 @@ class ItemsController<ApplicationController
   #   end
   # end
 
-  def destroy
-    item = Item.find(params[:id])
-    Review.where(item_id: item.id).destroy_all
-    item.destroy
-    redirect_to "/items"
-  end
+  # def destroy
+  #   item = Item.find(params[:id])
+  #   Review.where(item_id: item.id).destroy_all
+  #   item.destroy
+  #   redirect_to "/items"
+  # end
 
-  private
-
-  def item_params
-    params.permit(:name,:description,:price,:inventory,:image)
-  end
+  # private
+  #
+  # def item_params
+  #   params.permit(:name,:description,:price,:inventory,:image)
+  # end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ Rails.application.routes.draw do
 
   #admin
   namespace :admin do
-    get '/dashboard', to: "dashboard#index"
+    get '/', to: "dashboard#index"
     get '/users', to: "users#index"
     get '/users/:id', to: "users#show"
     get '/merchants', to: "merchants#index"
