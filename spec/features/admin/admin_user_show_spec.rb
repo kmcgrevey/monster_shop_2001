0a9677a77e5598978d448cdd14180dd73ce54911
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-15 14:10:47 -0600

    Completes US 54 -- admin sees user profile info

--- spec/features/admin/admin_user_show_spec.rb
+++ spec/features/admin/admin_user_show_spec.rb
@@ -26,10 +26,10 @@ RSpec.describe "As an Admin user", type: :feature do
     end
                       
     it "I see same info that user would see" do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       
-      visit "/admin/users/#{@user.id}"
-
+      visit "/profile"
+      
       within ".user-info" do
         expect(page).to have_content("#{@user.name}")
         expect(page).to have_content("#{@user.address}")
@@ -38,13 +38,46 @@ RSpec.describe "As an Admin user", type: :feature do
         expect(page).to have_content("#{@user.zip}")
         expect(page).to have_content("#{@user.email}")
         expect(page).to_not have_content("#{@user.password}")
+        expect(page).to have_link("My Orders")
+      end
+      
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      
+      visit "/admin/users/#{@user.id}"
+      
+      within ".user-info" do
+        expect(page).to have_content("#{@user.name}")
+        expect(page).to have_content("#{@user.address}")
+        expect(page).to have_content("#{@user.city}")
+        expect(page).to have_content("#{@user.state}")
+        expect(page).to have_content("#{@user.zip}")
+        expect(page).to have_content("#{@user.email}")
+        expect(page).to_not have_content("#{@user.password}")
+        expect(page).to have_link("My Orders")
+      end
+    end
+
+    it "I do not see a link to edit their profile or password" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      
+      visit "/profile"
+      
+      within ".user-info" do
+        expect(page).to have_link("Edit Profile")
+        expect(page).to have_link("Change Password")
+      end
+      
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      
+      visit "/admin/users/#{@user.id}"
+      
+      within ".user-info" do
+        expect(page).not_to have_link("Edit Profile")
+        expect(page).not_to have_button("Edit Profile")
+        expect(page).not_to have_link("Change Password")
+        expect(page).not_to have_button("Change Password")
       end
     end
 
   end
 end
-
-# As an admin user
-# When I visit a user's profile page ("/admin/users/5")
-# I see the same information the user would see themselves
-# I do not see a link to edit their profile
