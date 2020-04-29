b08fe87393618c296ef50b11b5c3da4c7b9f3563
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-28 20:12:07 -0600

    Adds item resource routes to namespaced merchant

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get "/", to: "welcome#index"
+  # get "/", to: "welcome#index" # <-- ORIGINAL
   root "welcome#index"
 
-  # resources :merchants
+  # resources :merchants # <-- ORIGINAL
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
   post "/merchants", to: "merchants#create"
   get "/merchants/:id", to: "merchants#show"
   delete "/merchants/:id", to: "merchants#destroy"
 
-  # resources :merchants do
+  # resources :merchants do # <-- ORIGINAL
   #   resources :items, only: [:index]
   # end
   get "/merchants/:merchant_id/items", to: "items#index"
 
-  # resources :items, only: [:index, :show] do
+  # resources :items, only: [:index, :show] do # <-- ORIGINAL
   #   resources :reviews, only: [:new, :create]
   # end
   get "/items", to: "items#index"
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   
   get "/items/:item_id/reviews/new", to: "reviews#new"
   post "/items/:item_id/reviews", to: "reviews#create"
-  # resources :reviews, only: [:edit, :update, :destroy]
+  # resources :reviews, only: [:edit, :update, :destroy] # <-- ORIGINAL
   get "/reviews/:id/edit", to: "reviews#edit"
   patch "/reviews/:id", to: "reviews#update"
   delete "/reviews/:id", to: "reviews#destroy"
@@ -42,7 +42,7 @@ Rails.application.routes.draw do
   patch "/cart/:item_id/:increment_decrement", to: "cart#increment_decrement"
 
   #orders
-  # resources :orders
+  # resources :orders # <-- ORIGINAL
   get "/orders/new", to: "orders#new"
   post "/orders", to: "orders#create"
   patch "/orders/:id", to: "orders#update"
@@ -64,13 +64,14 @@ Rails.application.routes.draw do
   #merchant-employee
   namespace :merchant do
     get "/", to: "dashboard#show"
-    get "/items", to: "items#index"
+    resources :items, only: [:index, :edit, :new, :destroy]
+    # get "/items", to: "items#index"
+    put "/items/:id", to: "items#update" # <-- ODD ROUTE WON'T RESOURCE
+    # delete "/items/:id", to: "items#destroy"
+    # get "/items/:id/edit", to: "items#edit"
+    # get "/items/new", to: "items#new"
+    post "/:merchant_id/items", to: "items#create" # <-- ODD ROUTE WON'T RESOURCE
     patch "/items/:id", to: "item_status#update"
-    put "/items/:id", to: "items#update"
-    delete "/items/:id", to: "items#destroy"
-    get "/items/:id/edit", to: "items#edit"
-    get "/items/new", to: "items#new"
-    post "/:merchant_id/items", to: "items#create"
     get "/orders/:order_id", to: "orders#show"
     patch "/orders/:order_id/:item_id", to: "orders#update"
   end
